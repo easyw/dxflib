ac16dd7aa964589c24fedc713619ea937d2fc022
Alastair McKinstry <mckinstry@debian.org>
2015-08-14 17:27:48 +0100

    Upstream 3.7.5

--- src/dl_codes.h
+++ src/dl_codes.h
@@ -1,13 +1,13 @@
 /****************************************************************************
-** Copyright (C) 2001-2011 RibbonSoft. All rights reserved.
+** Copyright (C) 2001-2013 RibbonSoft, GmbH. All rights reserved.
 ** Copyright (C) 2001 Robert J. Campbell Jr.
 **
 ** This file is part of the dxflib project.
 **
-** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.
+** This file is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
 **
 ** Licensees holding valid dxflib Professional Edition licenses may use 
 ** this file in accordance with the dxflib Commercial License
@@ -29,6 +29,8 @@
 
 #ifndef DXF_CODES_H
 #define DXF_CODES_H
+
+#include "dl_global.h"
 
 #if _MSC_VER > 1000
 #pragma once
@@ -62,7 +64,7 @@
 /**
  * Codes for colors and DXF versions.
  */
-class DL_Codes {
+class DXFLIB_EXPORT DL_Codes {
 public:
     /**
      * Standard DXF colors.
@@ -92,7 +94,10 @@ public:
      * Version numbers for the DXF Format.
      */
     enum version {
-        AC1009, AC1012, AC1014, AC1015
+        AC1009,         // R12
+        AC1012,
+        AC1014,
+        AC1015          // R2000
     };
 };
 
